2208aba74851be500264fc283124397ffec157e8
vuvietanhsp@gmail.com <you@example.com>
2023-07-22 04:35:39 +0700

    test

---
+++ RegMailRandom/tesst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegMailRandom
+{
+    internal class tesst
+    {
+        static void Main()
+        {
+            Console.WriteLine("Xin chao bạn");
+        }
+    }
+}
