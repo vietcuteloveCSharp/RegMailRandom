b99273277933be80e7901b3d05c4451c92008534
vuvietanhsp@gmail.com <you@example.com>
2023-07-22 08:25:17 +0700

    daycode thu

---
+++ RegMailRandom/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegMailRandom
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("tự tao đẩy code");
+        }
+    }
+}
